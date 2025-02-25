750560df6ac5bc47f6a949b261ef69b63c1d984b
xxkfqz <xxkfqz@gmail.com>
2025-02-25 11:37:20 +0500

    fix wildcards

--- process-list.sh
+++ process-list.sh
@@ -5,6 +5,6 @@ set -e
 LIST_NAME="ublacklist-nra.txt"
 TMP=$(mktemp)
 
-cat "$LIST_NAME" > "$TMP"
+perl -pe 's/^\*:\/\/(?!\*\.)/\*:\/\/\*\./g' < "$LIST_NAME" > "$TMP"
 sort < "$TMP" | uniq > "$LIST_NAME"
 git --no-pager diff "$LIST_NAME"
