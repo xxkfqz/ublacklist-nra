6575c53a062e0329a3dea192890d07c642f03654
xxkfqz <xxkfqz@gmail.com>
2025-08-09 11:18:34 +0500

    url clearing

--- process-list.sh
+++ process-list.sh
@@ -5,6 +5,7 @@ set -e
 LIST_NAME="ublacklist-nra.txt"
 TMP=$(mktemp)
 
+# Заменяем "*://domain" на "*://*.domain" и удаляем домены третьего уровня
 perl -pe 's/^\*:\/\/(?!\*\.)/\*:\/\/\*\./g' < "$LIST_NAME" | perl -pe 's/(?<=\.)(.+?)\.(.+?)\.(.+?)(?=\/)/\2\.\3/g' > "$TMP"
 sort < "$TMP" | uniq > "$LIST_NAME"
 git --no-pager diff "$LIST_NAME"
