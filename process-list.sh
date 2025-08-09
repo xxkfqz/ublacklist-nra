775b0598e2cb28c5bc68f9769b6d11e3f42d978f
xxkfqz <xxkfqz@gmail.com>
2025-08-09 11:16:58 +0500

    update

--- process-list.sh
+++ process-list.sh
@@ -5,6 +5,7 @@ set -e
 LIST_NAME="ublacklist-nra.txt"
 TMP=$(mktemp)
 
-perl -pe 's/^\*:\/\/(?!\*\.)/\*:\/\/\*\./g' < "$LIST_NAME" > "$TMP"
+perl -pe 's/^\*:\/\/(?!\*\.)/\*:\/\/\*\./g' < "$LIST_NAME" | perl -pe 's/(?<=\.)(.+?)\.(.+?)\.(.+?)(?=\/)/\2\.\3/g' > "$TMP"
 sort < "$TMP" | uniq > "$LIST_NAME"
 git --no-pager diff "$LIST_NAME"
+rm "$TMP"
